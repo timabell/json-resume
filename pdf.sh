17d9549bd27307a8d7444c6424c13438f771a17d
Tim Abell <tim@timwise.co.uk>
2023-07-19 22:07:33 +0100

    Set page size and margin
    - DRY paths
    - open once generated

--- pdf.sh
+++ pdf.sh
@@ -2,11 +2,23 @@
 # run ./setup.sh first
 # https://github.com/rbardini/resumed
 
-mkdir -p output
+outputPath=output
+filename=tim-abell-cv
+html="$outputPath/$filename.html"
+pdf="$outputPath/$filename.pdf"
+mkdir -p "$outputPath"
 
 # json to html
-npx resumed render resume.json --output output/tim-abell-cv.html --theme jsonresume-theme-kendall
+npx resumed render resume.json --output "$html" --theme jsonresume-theme-kendall
 
 # html to pdf
-input=`pwd`/output/tim-abell-cv.html
-npx chromehtml2pdf "file:///$input" --out=output/tim-abell-cv.pdf
+input="`pwd`/$html"
+npx chromehtml2pdf "file:///$input" --out="$pdf" \
+    --printBackground true \
+    --format A4 \
+    --marginTop 2cm \
+    --marginBottom 2cm \
+    --marginLeft 2cm \
+    --marginRight 2cm
+
+xdg-open "$pdf"
