e172b5f24abff6ae05bb8d86155b0b3745f8acd1
Tim Abell <tim@timwise.co.uk>
2024-05-15 11:05:51 +0100

    Generate json from hjson with node
    https://stackoverflow.com/questions/21194934/how-to-create-a-directory-if-it-doesnt-exist-using-node-js/26815894#26815894

--- buildcv.js
+++ buildcv.js
@@ -1,8 +1,27 @@
 #!/usr/bin/env node
 const {program} = require('commander')
+var hjson = require('hjson')
+var fs = require('fs')
 program.command('generate-preview')
     .action(() => {
-        console.log('mkpreview');
+        console.log('generating preview...');
+        if (!fs.existsSync( 'output')){
+            fs.mkdirSync( 'output');
+        }
+
+        fs.readFile('resume.hjson', 'utf8', function(err,cvhjson) {
+            if (err){
+                throw (err)
+            }
+            var cv = hjson.parse(cvhjson)
+            var cvjson = hjson.stringify(cv)
+            fs.writeFile("output/resume.json", cvjson, function(err){
+                if (err){
+                    throw (err)
+                }
+            })
+        })
+        console.log('done');
     })
 program.command('generate-auth')
     .option('--recruiter <recruiter>')
