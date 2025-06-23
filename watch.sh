d35c85acf46a602ebf8206ebc07a61c11f32e7ac
Tim Abell <t@0x5.uk>
2025-06-23 13:53:32 +0100

    Update watch.sh for new input path

--- watch.sh
+++ watch.sh
@@ -2,7 +2,7 @@
 
 # https://superuser.com/questions/181517/how-to-execute-a-command-whenever-a-file-changes/181543#181543
 
-while inotifywait -e close_write resume.hjson
+while inotifywait -e close_write input/resume.hjson
 do
     ./buildcv.js generate-preview
 done
